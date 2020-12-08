afb83750038f4f3db119bed534c51b8f8a5b0d49
jpranay08 <54579190+jpranay08@users.noreply.github.com>
2020-12-08 17:59:58 +0530

    Update settings.py

--- CapstoneELearning/settings.py
+++ CapstoneELearning/settings.py
@@ -77,13 +77,19 @@ WSGI_APPLICATION = 'CapstoneELearning.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': 'Capstone',
+    #    'USER': 'postgres',
+     #   'PASSWORD': 'admin',
+      #  'HOST':'localhost'
+    #}
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Capstone',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST':'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
